fb23a1677b9a0901d61034557f38d1377dde16a8
Iker Redondo <iker.redondo@yahooinc.com>
2024-09-16 20:10:42 +0100

    fix overlap

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,12 +3,12 @@ import Layout from '../components/Layout';
 import withAdminAuth from '../components/withAdminAuth';
 
 const DashboardPage = () => {
-  console.log('DashboardPage component rendered');
   return (
     <Layout>
       <DashboardComponent />
     </Layout>
   );
 };
+
 export default withAdminAuth(DashboardPage);
 
