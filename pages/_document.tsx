0deee0b8f87e3869f6473f72dfa195b7f09b90c4
Iker Redondo <iker.redondo@yahooinc.com>
2024-09-17 19:37:51 +0100

    fix PWA background

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#1F2937" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body>
         <Main />
